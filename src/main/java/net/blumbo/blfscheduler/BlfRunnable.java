04d7e96d79661a1736b9f657b10f53b0315c9461
Blumbo <blumboman@gmail.com>
2023-07-05 20:41:01 +0300

    Correct access modifier

--- src/main/java/net/blumbo/blfscheduler/BlfRunnable.java
+++ src/main/java/net/blumbo/blfscheduler/BlfRunnable.java
@@ -2,9 +2,9 @@ package net.blumbo.blfscheduler;
 
 public abstract class BlfRunnable implements BlfTask {
 
-    protected boolean isCancelled;
-    protected boolean isRepeating;
-    protected long period;
+    boolean isCancelled;
+    boolean isRepeating;
+    long period;
 
     /**
      * Stops a repeating task.
